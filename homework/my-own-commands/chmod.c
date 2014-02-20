5127cbd922d16dd176ce68d29658731e44255a04
Willy Husted <willy@willy-VirtualBox.(none)>
2014-02-20 10:14:12 -0800

    chmod data sanatized slightly

--- homework/my-own-commands/chmod.c
+++ homework/my-own-commands/chmod.c
@@ -6,28 +6,37 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/unistd.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
+
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
   //Accepts either 777 for full permissions or 444 for read only.
   //All calls should have all 3 numbers as arguments.
+  int errorCheck;
   if (argc < 3) {
-  	return -1;
+  	errorCheck = -1;
   }
 
   int input = atoi(argv[2]);
   
   // Read only
   if (input == 444) {
+      errorCheck = 0;
       syscall(15, argv[1], S_IRUSR | S_IRGRP | S_IROTH);
-  }
-
-  // Full permissions
-  if (input == 777) {
+  } else if (input == 777) {
+      errorCheck = 0;
       syscall(15, argv[1], S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+      
+  } else {
+      errorCheck = -1;
   }
 
-  return 0;
+  if (errorCheck == -1) {
+      printf("%s", "Please input a file followed by 444 or 777\n");
+      return -1;
+  } else {
+      return 0;
+  }
 }
