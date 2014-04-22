468bd9c951d56c1453ce14eebbc5aeb5abbaae35
whusted <whusted@lion.lmu.edu>
2014-04-21 21:04:05 -0700

    Skeleton functions added

--- homework/dining-philosophers/diningPhilosophers.c
+++ homework/dining-philosophers/diningPhilosophers.c
@@ -4,9 +4,18 @@
 
 #define PHILOSOPHERS 5
 
+void pickUpChopstick() {
+
+
+}
+
+void dropChopstick() {
+	
+}
+
 int main(int argc, char const *argv[]) {
 
 	pthread_t philosophers[PHILOSOPHERS];
-	
+
 	return 0;
 }
