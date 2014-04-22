359dfec419d060d52fd1d1c744fe33192aa5fc24
whusted <whusted@lion.lmu.edu>
2014-04-21 21:13:10 -0700

    threads and mutex determined

--- homework/dining-philosophers/diningPhilosophers.c
+++ homework/dining-philosophers/diningPhilosophers.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 
 #define PHILOSOPHERS 5
+#define THINKING 0
+#define EATING 1
 
 void pickUpChopstick() {
 
@@ -10,12 +12,17 @@ void pickUpChopstick() {
 }
 
 void dropChopstick() {
-	
+
 }
 
 int main(int argc, char const *argv[]) {
 
+	// Chopsticks are shared resource
+	pthread_mutex_t chopsticks[PHILOSOPHERS];
+
+    // Philosophers are the threads
 	pthread_t philosophers[PHILOSOPHERS];
+	int philosopher_condition[PHILOSOPHERS];
 
 	return 0;
 }
