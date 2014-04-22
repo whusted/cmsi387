afd2a9ae0653b0d2b292c02df2a5bccedcbc2676
whusted <whusted@lion.lmu.edu>
2014-04-21 22:58:10 -0700

    Verb functions implemented, variables moved to 'static' position -- need to clean up later

--- homework/dining-philosophers/diningPhilosophers.c
+++ homework/dining-philosophers/diningPhilosophers.c
@@ -7,31 +7,48 @@
 #define EATING 1
 #define HUNGRY 2
 
-void pickUpChopstick() {
+// Chopsticks are shared resource
+pthread_mutex_t chopsticks[PHILOSOPHERS];
+int chopstick_condition[PHILOSOPHERS];
+
+int philosopher_condition[PHILOSOPHERS];
+
+void pickUpChopstick(int chopstick) {
   //lock mutex
+  pthread_mutex_lock(&chopsticks[chopstick]);
+  // Check if current chopstick is already picked up
+  // Bump current chopstick state by 1
+  chopstick_condition[chopstick] += 1;
 
 }
 
-void dropChopstick() {
+void dropChopstick(int chopstick) {
   //unlock mutex
+  pthread_mutex_unlock(&chopsticks[chopstick]);
+  chopstick_condition[chopstick] -= 1;
 }
 
-void eat() {
-  // pick up chopsticks one at a time
-  // set current philosopher condtion to eating 
-  // philosopher_condition[] = EATING;
+void eat(int philosopher) {
+  pickUpChopstick(philosopher);
+  pickUpChopstick((philosopher + 1) % PHILOSOPHERS);
+  
+  // set current philosopher condtion to eating
+  philosopher_condition[philosopher] = EATING;
   // eat for random time
   randomWait(10);
 }
 
-void think() {
+void think(int philosopher) {
   // think for random time
   randomWait(10);
+  philosopher_condition[philosopher] = HUNGRY;
   // philosopher condition should change to hungry... meaning a 3rd function is needed
 }
 
 void finishedEating() {
   // should be called when philosopher condition is eating
+  // drop chopsticks individually
+
   // philosopher condition should change to thinking
 }
 
@@ -44,26 +61,21 @@ int randomWait(int bound) {
 
 int main(int argc, char const *argv[]) {
 
-	// Chopsticks are shared resource
-  pthread_mutex_t chopsticks[PHILOSOPHERS];
-	int chopstick_condition[PHILOSOPHERS];
-
     // Philosophers are the threads
 	pthread_t philosophers[PHILOSOPHERS];
-	int philosopher_condition[PHILOSOPHERS];
 
 	// Now make them eat and think until the end of time
 	//while (1) {
-      //print the states of the philosophers
-      //printPhilosophers();
+    //print the states of the philosophers
+    //printPhilosophers();
 
-      // if (philosopher_condition[current] == THINKING) {
-      //   think();
-      // } else if (philosopher_condition[current] == EATING) {
-      //   eat();
-      // } else if (philosopher_condition[current] == HUNGRY) {
-      //   //be hungry? not sure what to call this function
-      // }
+    // if (philosopher_condition[current] == THINKING) {
+    //   think();
+    // } else if (philosopher_condition[current] == EATING) {
+    //   eat();
+    // } else if (philosopher_condition[current] == HUNGRY) {
+    //   finishedEating()
+    // }
 
 
 
